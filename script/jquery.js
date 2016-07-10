97ca816d0093c04a150c2478be3782ae4e2400d0
Sandra Ptaszek <ptaszek.sandra@gmail.com>
2016-07-10 15:43:50 +0200

    Add slower hover effect to logo

--- script/jquery.js
+++ script/jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	console.log('halo')
 	$('.logo').mouseenter(function(){
-			$(this).effect('bounce', { times: 2 } , 1000);
+			$(this).effect('bounce', { times: 1 } , 800);
 		});
 });
